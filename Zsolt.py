35dcbd2f0f02bd56f5f9bed9059bda6357a80664
leskoarpi <leskoa47@gmail.com>
2021-01-16 10:57:27 +0000

    kesz

--- Zsolt.py
+++ Zsolt.py
@@ -4,8 +4,8 @@ def valami():
   t.speed(0)
   t.hideturtle()
   t.pendown()
-  for x in range (50):
-    t.circle(80,90)
+  for x in range (80):
+    t.circle(100,110)
     if x % 2 == 0:
-      t.circle(60,100)
+      t.circle(80,120)
 valami()
